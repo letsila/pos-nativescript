7809479b6e0c9d43b4a2245e4d8bec1b08d64efd
letsila <t.razafinirina@gmail.com>
2017-05-22 09:33:28 +0300

    NativeScript installation

---
+++ app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "my-app",
+  template: `
+    <ActionBar title="Order" class="action-bar"></ActionBar>
+  `
+})
+export class AppComponent {
+  constructor() {
+
+  }
+}
